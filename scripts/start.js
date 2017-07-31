677ec27ecfc0e3d0118905928fef035cac70ab0f
crazyquark <cristian.sandu@gmail.com>
2017-07-31 14:38:07 +0300

    allow different server address

--- scripts/start.js
+++ scripts/start.js
@@ -322,7 +322,7 @@ function addToStatus(dappUrl) {
 
 function run(port) {
   var protocol = process.env.HTTPS === 'true' ? "https" : "http";
-  var host = 'localhost'; // we don't support other hosts at this moment
+  var host = process.env.SERVER || 'localhost'; // we don't support other hosts at this moment
   setupCompiler(host, port, protocol);
   runDevServer(host, port, protocol);
 }
