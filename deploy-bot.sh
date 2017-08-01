c998c9b55ff9c98bb903ab51e4af7ffbbab7ee88
crazyquark <cristian.sandu@gmail.com>
2017-08-01 13:59:51 +0300

    easily deploy bot from nginx

---
+++ deploy-bot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#status-dev-cli add '{"whisper-identity": "status-eliza", "bot-url": "http://10.162.130.120:7878/bot/bot.js", "name": "Eliza"}' --ip 10.162.131.105
+status-dev-cli add --botUrl http://10.162.130.120:7878/bot/bot.js --ip 10.162.131.105
+
