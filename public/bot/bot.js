758c9495d9ed8d637965cd9ac18624e55b811c33
crazyquark <cristian.sandu@gmail.com>
2017-08-03 16:46:32 +0300

    working on the confirm command

--- public/bot/bot.js
+++ public/bot/bot.js
@@ -1,25 +1,3 @@
-status.command({
-    name: "hello",
-    title: "HelloBot",
-    description: "Helps you say hello",
-    color: "#CCCCCC",
-    preview: function (params) {
-        var text = status.components.text(
-            {
-                style: {
-                    marginTop: 5,
-                    marginHorizontal: 0,
-                    fontSize: 14,
-                    fontFamily: "font",
-                    color: "black"
-                }
-            }, "Hello from the other side!");
-        return { markup: status.components.view({}, [text]) };
-    }
-});
-
-var eliza = {};
-
 // Created with Remix IDE and Metamask on Ropsten
 var contractAddress = '0x03749a095ec23d8108f7338b09ced562db0195b7';
 var contractOwner = '0x53022f4f4e8672c56499eb6b69bf62d727b6d071';
@@ -81,9 +59,8 @@ function getMyPendingTxs(result) {
 
 status.addListener('init', function (params, context) {
     status.sendMessage('G\'day');
-    status.sendMessage('I recognize keywords in chat like: balance, account, status, transaction IDs');
-    status.sendMessage('For everything else, my friend Eliza will be as unhelpful as possible :)')
-    eliza.start();
+    status.sendMessage('I recognize keywords in chat like: balance, account, status and transaction numbers');
+    status.sendMessage('For everything else, I will be as unhelpful as possible :)')
 });
 
 status.addListener('on-message-send', function (params, context) {
@@ -120,10 +97,74 @@ status.addListener('on-message-send', function (params, context) {
                 result['text-message'] = 'I am not aware of a transaction with the ID ' + txid;
             }
         } else {
-            // Take it away Eliza
-            result['text-message'] = eliza.reply(message);
+            result['text-message'] = 'I am not smart enough to answer that :('
         }
     }
 
     return result;
 });
+
+status.command({
+     name: "greet",
+     title: "Greeter",
+     description: "Helps you choose greetings",
+     color: "#0000ff",
+     params: [{
+              name: "greet",
+              type: status.types.TEXT,
+              suggestions: helloSuggestions
+             }]
+ })
+
+function suggestionsContainerStyle(suggestionsCount) {
+    return {
+        marginVertical: 1,
+        marginHorizontal: 0,
+        keyboardShouldPersistTaps: "always",
+        height: Math.min(150, (56 * suggestionsCount)),
+        backgroundColor: "white",
+        borderRadius: 5,
+        flexGrow: 1
+    };
+}
+var suggestionSubContainerStyle = {
+    height: 56,
+    borderBottomWidth: 1,
+    borderBottomColor: "#0000001f"
+};
+
+var valueStyle = {
+    marginTop: 9,
+    fontSize: 14,
+    fontFamily: "font",
+    color: "#000000de"
+};
+
+function helloSuggestions() {
+    var suggestions = ["Hello", "Goodbye"].map(function(entry) {
+        return status.components.touchable(
+            {onPress: status.components.dispatch([status.events.SET_VALUE, entry])},
+            status.components.view(
+                suggestionsContainerStyle,
+                [status.components.view(
+                    suggestionSubContainerStyle,
+                    [
+                        status.components.text(
+                            {style: valueStyle},
+                            entry
+                        )
+                    ]
+                )]
+            )
+        );
+    });
+
+    // Let's wrap those two touchable buttons in a scrollView
+    var view = status.components.scrollView(
+        suggestionsContainerStyle(2),
+        suggestions
+    );
+
+    // Give back the whole thing inside an object.
+    return {markup: view};
+}
